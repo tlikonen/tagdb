aa23ec5bf7a6ae627ac48054f93599686f283220
Teemu Likonen <tlikonen@iki.fi>
2015-01-16 22:51:41 +0200

    Update copyright year

--- tagdb.lisp
+++ tagdb.lisp
@@ -1,7 +1,7 @@
 ;;;; Tagdb --- Tag-based command-line database tool
 
 
-;;; Copyright (C) 2014 Teemu Likonen <tlikonen@iki.fi>
+;;; Copyright (C) 2015 Teemu Likonen <tlikonen@iki.fi>
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
