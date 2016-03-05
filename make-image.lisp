af15eae815d42f44dd3b4a29e1ca0b1e1a729910
Teemu Likonen <tlikonen@iki.fi>
2016-03-05 09:12:14 +0200

    Update copyright year

--- make-image.lisp
+++ make-image.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (C) 2014-2015 Teemu Likonen <tlikonen@iki.fi>
+;;; Copyright (C) 2014-2016 Teemu Likonen <tlikonen@iki.fi>
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
