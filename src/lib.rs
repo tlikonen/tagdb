51a9491f314f03b8038f7184c844562d2e3080c0
Teemu Likonen <tlikonen@iki.fi>
2025-07-18 09:33:52 +0300

    Add logic for parsing command options

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,23 @@
 use std::error::Error;
 
-#[derive(Default)]
 pub struct Modes {
     pub verbose: bool,
     pub quiet: bool,
 }
 
-pub async fn run(_modes: Modes, _args: &[String]) -> Result<(), Box<dyn Error>> {
+pub enum Operation {
+    Normal,
+    Short,
+    Count,
+    Create,
+    Edit,
+    List,
+    Reassociate,
+    Help,
+    Version,
+}
+
+pub async fn run(_modes: Modes, _op: Operation, _args: &[String]) -> Result<(), Box<dyn Error>> {
     eprintln!("run");
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use just_getopt::{Args, OptFlags, OptSpecs, OptValue};
 use std::{error::Error, process::ExitCode};
-use tagdb::Modes;
+use tagdb::{Modes, Operation};
 
 static PROGRAM_NAME: &str = env!("CARGO_PKG_NAME");
 static PROGRAM_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -12,6 +12,12 @@ async fn main() -> ExitCode {
     let args = OptSpecs::new()
         .option("quiet", "q", OptValue::None)
         .option("verbose", "v", OptValue::None)
+        .option("short", "s", OptValue::None)
+        .option("count", "n", OptValue::None)
+        .option("create", "c", OptValue::None)
+        .option("edit", "e", OptValue::None)
+        .option("list", "l", OptValue::None)
+        .option("reassociate", "r", OptValue::None)
         .option("help", "h", OptValue::None)
         .option("version", "version", OptValue::None)
         .flag(OptFlags::PrefixMatchLongOptions)
@@ -34,23 +40,6 @@ async fn main() -> ExitCode {
         return ExitCode::FAILURE;
     }
 
-    if args.option_exists("help") {
-        println!(
-            "Usage: {PROGRAM_NAME} [options] [--] TAG ...\n\n{txt}",
-            txt = include_str!("usage.txt")
-        );
-        return ExitCode::SUCCESS;
-    }
-
-    if args.option_exists("version") {
-        println!(
-            "{PROGRAM_NAME} v{PROGRAM_VERSION}\n\
-             Author:  {PROGRAM_AUTHORS}\n\
-             License: {PROGRAM_LICENSE}"
-        );
-        return ExitCode::SUCCESS;
-    }
-
     match config_stage(args).await {
         Ok(_) => ExitCode::SUCCESS,
         Err(e) => {
@@ -66,5 +55,71 @@ async fn config_stage(args: Args) -> Result<(), Box<dyn Error>> {
         quiet: args.option_exists("quiet"),
     };
 
-    tagdb::run(modes, &args.other).await
+    let mut commands = 0;
+    let mut op = Operation::Normal;
+
+    if args.option_exists("short") {
+        op = Operation::Short;
+        commands += 1;
+    }
+
+    if args.option_exists("count") {
+        op = Operation::Count;
+        commands += 1;
+    }
+
+    if args.option_exists("create") {
+        op = Operation::Create;
+        commands += 1;
+    }
+
+    if args.option_exists("edit") {
+        op = Operation::Edit;
+        commands += 1;
+    }
+
+    if args.option_exists("list") {
+        op = Operation::List;
+        commands += 1;
+    }
+
+    if args.option_exists("reassociate") {
+        op = Operation::Reassociate;
+        commands += 1;
+    }
+
+    if args.option_exists("help") {
+        op = Operation::Help;
+        commands += 1;
+    }
+
+    if args.option_exists("version") {
+        op = Operation::Version;
+        commands += 1;
+    }
+
+    if commands > 1 {
+        Err("Only one command option is allowed. Use option “-h” alone for help.")?;
+    }
+
+    match op {
+        Operation::Help => {
+            println!(
+                "Usage: {PROGRAM_NAME} [options] [--] TAG ...\n\n{txt}",
+                txt = include_str!("usage.txt")
+            );
+            Ok(())
+        }
+
+        Operation::Version => {
+            println!(
+                "{PROGRAM_NAME} v{PROGRAM_VERSION}\n\
+                 Author:  {PROGRAM_AUTHORS}\n\
+                 License: {PROGRAM_LICENSE}"
+            );
+            Ok(())
+        }
+
+        operation => tagdb::run(modes, operation, &args.other).await,
+    }
 }
